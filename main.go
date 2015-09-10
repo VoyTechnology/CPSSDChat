469e79dd79a4be4c1ac80117074be097ff281edf
Terry Bolt <bolt.terry@gmail.com>
2015-09-11 00:29:39 +0100

    Simple but should work. Needs a client to create a websocket connection to test it. That is not my area of expertise.

--- main.go
+++ main.go
@@ -8,44 +8,50 @@ package main
 import (
 	"github.com/gorilla/websocket"
 	"log"
+	"net/http"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 type room struct {
 	// All active connections
-	connections map[*websocket.Conn]bool
+	connections map[*connection]bool
 
 	// A buffered channel of incoming messages
 	messages chan string
 
 	// Requests to join the room
-	register chan *websocket.Conn
+	register chan *connection
 
 	// Requests to leave the room
-	unregister chan *websocket.Conn
+	unregister chan *connection
 }
 
 func newRoom() *room {
 	return &room{
-		connections: make(map[*websocket.Conn]bool),
+		connections: make(map[*connection]bool),
 		messages:    make(chan string, 20),
-		register:    make(chan *websocket.Conn),
-		unregister:  make(chan *websocket.Conn),
+		register:    make(chan *connection),
+		unregister:  make(chan *connection),
 	}
 }
 
 func (r *room) run() {
 	for {
 		select {
-		case conn := <-register:
+		case conn := <-r.register:
 			r.connections[conn] = true
-		case conn := <-unregister:
-			if _, ok := r.connections[c]; ok {
-				delete(r.connections, c)
+		case conn := <-r.unregister:
+			if _, ok := r.connections[conn]; ok {
+				delete(r.connections, conn)
 			}
 		// When receiving a message, send it down all active connections
-		case message := <-messages:
+		case message := <-r.messages:
 			for conn := range r.connections {
-				err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+				err := conn.ws.WriteMessage(websocket.TextMessage, []byte(message))
 				if err != nil {
 					log.Printf("ERROR: Could not write message: %s\n", err)
 				}
@@ -54,6 +60,43 @@ func (r *room) run() {
 	}
 }
 
+type connection struct {
+	ws *websocket.Conn
+	r  *room
+}
+
+func (conn *connection) reader() {
+	for {
+		_, message, err := conn.ws.ReadMessage()
+		if err != nil {
+			log.Printf("ERROR: Could not read message: %s\n", err)
+			break
+		}
+		conn.r.messages <- string(message)
+	}
+	conn.ws.Close()
+}
+
+type websocketHandler struct {
+	r *room
+}
+
+func (handler websocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("ERROR: Could not upgrade websocket: %s\n", err)
+	}
+	conn := &connection{ws: ws, r: handler.r}
+	handler.r.register <- conn
+	defer func() { handler.r.unregister <- conn }()
+}
+
 func main() {
-	log.Fatalln("I do nothing!")
+	r := newRoom()
+	go r.run()
+	http.Handle("/connect", websocketHandler{r: r})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln("FATAL: ", err)
+	}
 }
